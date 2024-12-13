d3c27ce9bf21826a97a93832ef5040952440bf26
Semyon <semenb668@gmail.com>
2024-12-13 10:21:53 +0500

    написал функцию get_weather

--- hw_23.py
+++ hw_23.py
@@ -4,3 +4,8 @@ from plyer import notification
 
 API_KEY = r'b7a99c8304c6cb7c05b09085adceea09'
 CITY = 'Ханты-Мансийск'
+
+def get_weather(city: str, api_key: str) -> dict:
+    url = fr'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru'
+    response = requests.get(url)
+    return response.json()
